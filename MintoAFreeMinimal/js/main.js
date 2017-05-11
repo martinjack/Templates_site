0148fae65fb56780f014d8fce6c9c9271d941f71
martinjack <killfess@gmail.com>
2017-05-11 12:30:55 +0300

    MintoAFreeMinimal

---
+++ MintoAFreeMinimal/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	var swiper = new Swiper ('.swiper-container', {
+	    direction: 'horizontal',
+	    loop: true,
+	    nextButton: '.swiper-button-next',
+    	prevButton: '.swiper-button-prev',
+    	autoplay: 5000
+	})        
+});
