615fd3496ddde0a276af37af4465a39f5c009452
martinjack <killfess@gmail.com>
2017-05-15 21:44:19 +0300

    Add files via upload

--- Uisumo_Rioevent/js/main.js
+++ Uisumo_Rioevent/js/main.js
@@ -6,9 +6,31 @@ $(document).ready(function(){
 			var hash = this.hash;
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
-			}, 800, function(){
+			}, 2000, function(){
 				window.location.hash = hash;
 			});
 		}
 	});
+	var swiper = new Swiper('.swiper-container', {
+		direction: 'horizontal',
+		loop: true,
+		pagination: '.swiper-pagination',
+        paginationClickable: true,
+        autoplay: 5000
+	});
+	// $("a[data-toggle='collapse']").click(function(){
+	// 	if($(this).hasClass("collapsed")){
+	// 		$(this).parent().parent().addClass("active-acco");
+	// 		console.log($(this).parent().parent());
+	// 	}else{
+	// 		$(".card-header").removeClass("active-acco");
+	// 	}
+		
+	// });
+	// $("#accordion").on("shown.bs.collapse", function(){
+	// 	$(".card-header").each(function(){
+	// 		$(this).removeClass("active-acco");
+	// 	});
+	// 	$(".card-header").addClass("active-acco");
+	// });
 });
