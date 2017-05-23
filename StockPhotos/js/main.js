319e9821e57e694669895d99e465bb29ba167ff6
martinjack <killfess@gmail.com>
2017-05-23 16:56:04 +0300

    Update js

--- StockPhotos/js/main.js
+++ StockPhotos/js/main.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
 	var slider = new Swiper('.swiper-container',{
 		direction: 'horizontal',
-		loop: true
+		loop: true,
+		prevButton: '.swiper-button-prev',
+		nextButton: '.swiper-button-next',
+		autoplay: 5000
 	});
 });
