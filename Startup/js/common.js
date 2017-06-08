dc2a11be4972c9b3e1f5d280b1525a5a06f28d7d
martinjack <killfess@gmail.com>
2017-06-08 12:25:27 +0300

    Update template

--- Startup/js/common.js
+++ Startup/js/common.js
@@ -7,4 +7,23 @@ $(document).ready(function(){
 	    live:         true       
     });
     wow.init();
+    var swiper = new Swiper('.creator-swiper', {
+        slidesPerView: 4,
+        paginationClickable: true,
+        spaceBetween: 30,
+        autoplay: 5000
+    });
+    var clienSwipe = new Swiper('.clients-swiper',{
+    	pagination: '.swiper-pagination',
+    	autoplay: 5000
+    });
+    $(window).resize(function(){
+        var ww = $(window).width();
+        if(ww > 1200) swiper.params.slidesPerView = 4;
+        if(ww < 1200) swiper.params.slidesPerView = 3;
+        if(ww < 992)  swiper.params.slidesPerView = 2;
+        if(ww < 768)  swiper.params.slidesPerView = 1;
+        swiper.update();
+    });
+    $(window).trigger('resize');
 });
